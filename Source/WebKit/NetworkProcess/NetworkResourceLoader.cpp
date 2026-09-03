707cc3b784fd2f9db7c5eb78db4f0fbe4a323f16
Frédéric Wang Nélar <fwang@igalia.com>
2026-09-03 10:12:20 +0200

    fixup! Implement HTTP-network compression-dictionary fetch and libsoup support https://bugs.webkit.org/show_bug.cgi?id=295249
    Allow same-origin no-cors requests.

--- Source/WebKit/NetworkProcess/NetworkResourceLoader.cpp
+++ Source/WebKit/NetworkProcess/NetworkResourceLoader.cpp
@@ -492,7 +492,8 @@ void NetworkResourceLoader::startNetworkLoad(ResourceRequest&& request, FirstLoa
     CheckedPtr networkStorageSession = connectionToWebProcess().networkProcess().storageSession(sessionID());
     ASSERT(networkStorageSession);
     if (connectionToWebProcess().compressionDictionaryEnabled()
-        && canUseCache(request) && m_parameters.options.mode != FetchOptions::Mode::NoCors
+        && canUseCache(request)
+        && (m_parameters.options.mode != FetchOptions::Mode::NoCors || (m_networkLoadChecker && m_networkLoadChecker->isSameOriginRequest()))
         && !networkStorageSession->shouldBlockCookies(originalRequest().firstPartyForCookies(), originalRequest().url(), frameID(), pageID(), ShouldRelaxThirdPartyCookieBlocking::No, IsKnownCrossSiteTracker::No)
         && SecurityOrigin::isSecure(request.url())) {
         // 6. Let compressionDictionaryCache be the result of determining the compression-dictionary cache partition given request.
